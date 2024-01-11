a177c469758c9c087419710f413f84418fbdd4e1
Mickael Brothier <mickaelbrothier@outlook.com>
2024-01-11 12:01:15 +0100

    Fisrt commit

---
+++ page.py
@@ -0,0 +1,233 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import requests
+import json
+from config import BEARER_TOKEN
+from werkzeug.utils import secure_filename
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+from collections import defaultdict
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'csv', 'xlsx', 'xls'}
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prenom = db.Column(db.String(50), nullable=False)
+    nom = db.Column(db.String(50), nullable=False)
+    sexe = db.Column(db.String(10), nullable=False)
+    pseudo = db.Column(db.String(50), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route('/reponse_formulaire', methods=['POST'])
+def rep_form():
+    sexe = request.form['sexe']
+    prenom = request.form['prenom']
+    nom = request.form['nom']
+    pseudo = request.form['pseudo']
+           
+    existing_user = User.query.filter_by(pseudo=pseudo).first()
+
+    if existing_user:
+        return render_template("pseudo_exists.html", pseudo=pseudo)
+
+    if sexe == "Homme":
+        titre = "Mr"
+    else:
+        titre = "Mme"
+
+    message = f"{titre} {prenom} {nom}, votre nom d'utilisateur est {pseudo}"
+    
+    user = User(prenom=prenom, nom=nom, sexe=sexe, pseudo=pseudo)
+    db.session.add(user)
+    db.session.commit()
+
+    return render_template("page2.html", message=message)
+
+
+@app.route('/inscrits', methods=['GET'])
+def users():
+    all_users = User.query.all()
+    return render_template("inscrits.html", users=all_users)
+
+@app.route("/form_actualite")
+def form_actu():
+    return render_template("form_actualite.html")
+
+@app.route('/actualites', methods=['POST'])
+def get_news():
+    entreprise_symbole = request.form.get('entreprise_symbole')
+
+    if not entreprise_symbole:
+        return render_template("erreur.html", message="Veuillez entrer le symbole de l'entreprise.")
+
+    url = 'https://devapi.ai/api/v1/markets/news'
+    params = {
+        'ticker': entreprise_symbole,
+    }
+    headers = {
+        'Authorization': f'Bearer {BEARER_TOKEN}'
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        news_data = response.json()
+        
+        articles = news_data.get('body', [])
+
+        return render_template("actualites.html", entreprise_symbole=entreprise_symbole, articles=articles)
+    else:
+        return render_template("erreur.html", message=f"Erreur lors de la récupération des actualités. Code d'état : {response.status_code}")
+
+@app.route("/form_bourse")
+def form_bourse():
+    return render_template("form_bourse.html") 
+
+@app.route('/bourses', methods=['POST'])
+def get_bourse():
+    entreprise_symbole = request.form.get('entreprise_symbole') 
+
+    if not entreprise_symbole:
+        return render_template("erreur.html", message="Veuillez entrer le symbole de l'entreprise.")
+
+    url = 'https://devapi.ai/api/v1/markets/quote'
+    params = {
+        'ticker': entreprise_symbole,
+    }
+    headers = {
+        'Authorization': f'Bearer {BEARER_TOKEN}'
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        news_data = response.json()
+        
+        quotes = news_data.get('body', [])
+        primary_data = news_data.get('primaryData', {}) 
+ 
+        return render_template("bourses.html", entreprise_symbole=entreprise_symbole, quotes=quotes, primary_data=primary_data)
+    else:
+        return render_template("erreur.html", message=f"Erreur lors de la récupération des informations sur la bourse. Code d'état : {response.status_code}")
+
+
+def stats(file_path):
+    try:
+        df = pd.read_excel(file_path) if file_path.endswith('.xlsx') or file_path.endswith('.xls') else pd.read_csv(file_path)
+        statistics = df.describe()
+        return statistics.to_html()
+    except Exception as e:
+        return f"Erreur: {str(e)}"
+    
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/download')
+def download():
+    return render_template('download.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        statistics = stats(file_path)
+
+        # Remove uploaded file after getting statistics
+        os.remove(file_path)
+
+        return render_template('download.html', statistics=statistics)
+    else:
+        return 'Format de fichier invalide. Veuillez télécharger un fichier CSV ou Excel.'
+
+@app.route('/stocks')
+def stocks():
+    return render_template('stocks.html')
+
+from datetime import datetime
+
+@app.route('/history', methods=['POST'])
+def history():
+    entreprise_symbole = request.form.get('entreprise_symbole')
+    interval = request.form.get('interval') 
+
+    if not entreprise_symbole:
+        return render_template("erreur.html", message="Veuillez entrer le symbole de l'entreprise et la période.")
+
+    url = 'https://devapi.ai/api/v1/markets/stock/history'
+    params = {
+        'ticker': entreprise_symbole,
+        'interval': interval,
+    }
+    headers = {
+        'Authorization': f'Bearer {BEARER_TOKEN}'
+    }
+    response = requests.request('GET', url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        news_data = response.json()
+        
+        prices = news_data.get('body', [])
+
+        aggregated_prices = defaultdict(list)
+        for entry in prices.values():
+            date_str = entry.get('date')
+            close = entry.get('close')
+            
+            date_obj = datetime.strptime(date_str, "%d-%m-%Y")
+
+            # Extraire le mois de la date
+            month_key = date_obj.strftime('%Y-%m')
+            
+            aggregated_prices[month_key].append(close)
+
+        month_labels = list(aggregated_prices.keys())
+        close_values = [sum(prices) / len(prices) for prices in aggregated_prices.values()]
+ 
+        plt.figure(figsize=(12, 7))
+        plt.plot(month_labels, close_values, label='Close Price')
+        plt.ylabel('Close Price')
+        plt.xticks(rotation=90)
+        plt.legend()
+        
+        img_data = BytesIO()
+        plt.savefig(img_data, format='png')
+        img_data.seek(0)
+        plt.close()
+
+        base64_img = base64.b64encode(img_data.read()).decode('utf-8')
+
+        return render_template("stocks_graph.html", entreprise_symbole=entreprise_symbole, base64_img=base64_img)
+    else:
+        return render_template("erreur.html", message=f"Erreur lors de la récupération des informations sur la bourse. Code d'état : {response.status_code}")
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
---
+++ train.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import numpy as np
+
+import os
+import cv2
+from PIL import Image
+
+from tqdm.auto import trange, tqdm
+
+import keras
+import tensorflow as tf
+from tqdm.keras import TqdmCallback
+
+from matplotlib import pyplot as plt
+from ipywidgets import interact
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+
+from keras.preprocessing.image import ImageDataGenerator
+
+
+train = pd.read_csv('data/train.csv')
+test = np.genfromtxt('data/test.csv', delimiter=',', skip_header=1)
+sample_submission = pd.read_csv('data/sample_submission.csv')
+
+train.shape, test.shape, sample_submission.shape
+
+labels = train['label']
+train.drop(['label'], axis=1, inplace=True)
+
+train = np.array(train, dtype=float)
+
+train = train.reshape(42000, 28, 28)
+test = test.reshape(28000, 28, 28)
+
+img_rows, img_cols = (32, 32)
+input_shape = (img_rows, img_cols, 3)
+
+def resizeImage(images, new_size):
+    nimages = np.zeros((images.shape[0], new_size[0], new_size[1], 3))
+    for image in trange(images.shape[0]):
+        nimages[image, :new_size[0], :new_size[1], 0] = cv2.resize(images[image], new_size)
+        nimages[image, :new_size[0], :new_size[1], 1] = cv2.resize(images[image], new_size)
+        nimages[image, :new_size[0], :new_size[1], 2] = cv2.resize(images[image], new_size)
+    return nimages
+
+train = resizeImage(train, (img_rows, img_cols))
+test = resizeImage(test, (img_rows, img_cols))
+
+val_split = 0.05
+BATCH_SIZE = 256
+TRAIN_STEPS_PER_EPOCH = train.shape[0]*(1-val_split)//BATCH_SIZE
+VAL_STEPS_PER_EPOCH = train.shape[0]*val_split//BATCH_SIZE
+
+train_datagen = ImageDataGenerator(rescale=1/255.0,
+                                   rotation_range=15,
+                                   zoom_range=0.15,
+                                   validation_split=val_split,
+                                   width_shift_range=0.15,
+                                   height_shift_range=0.15,
+                                   shear_range=0.15,
+                                   fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1/255.0)
+
+val_split = 0.05
+BATCH_SIZE = 256
+TRAIN_STEPS_PER_EPOCH = train.shape[0]*(1-val_split)//BATCH_SIZE
+VAL_STEPS_PER_EPOCH = train.shape[0]*val_split//BATCH_SIZE
+
+train_aug = train_datagen.flow(train,
+                               labels,
+                               batch_size=BATCH_SIZE,
+                               subset='training',
+                               shuffle=False,
+                               seed=42)
+
+valid_aug = train_datagen.flow(train,
+                               labels,
+                               batch_size=BATCH_SIZE,
+                               subset='validation',
+                               shuffle=False,
+                               seed=42)
+
+
+test_aug = train_datagen.flow(test,
+                              batch_size=BATCH_SIZE,
+                              shuffle=False,
+                              seed=42)
+
+def build_model():
+    cnn = keras.models.Sequential()
+
+    cnn.add(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='random_uniform', input_shape=input_shape))
+    cnn.add(keras.layers.Dropout(0.5))
+    cnn.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
+
+    cnn.add(keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='random_uniform'))
+    cnn.add(keras.layers.Dropout(0.5))
+    cnn.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
+
+    cnn.add(keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='random_uniform'))
+    cnn.add(keras.layers.Dropout(0.5))
+    cnn.add(keras.layers.MaxPool2D(pool_size=(3, 3)))
+
+    cnn.add(keras.layers.Flatten())
+    cnn.add(keras.layers.Dense(units=128, activation='relu'))
+
+    cnn.add(keras.layers.Dense(units=10, activation='softmax'))
+    
+    cnn.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['acc'])
+    return cnn
+
+cnn = build_model()
+
+#keras.utils.plot_model(cnn, show_shapes=True, show_layer_names=True)
+
+ch = tf.keras.callbacks.ModelCheckpoint(
+    filepath='mnist_model.h5',
+    save_weights_only=False,
+    monitor='acc',
+    mode='max',
+    save_best_only=True
+)
+
+es = tf.keras.callbacks.EarlyStopping(
+    monitor='acc',
+    min_delta=0.003,
+    patience=15,
+    mode='max',
+    restore_best_weights=True,
+)
+
+lr = tf.keras.callbacks.ReduceLROnPlateau(
+    monitor='acc',
+    factor=0.05,
+    patience=3,
+    mode='max',
+)
+
+cnn.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
+
+history = cnn.fit(train_aug, validation_data=valid_aug, epochs=50, verbose=0,
+                  callbacks=[ch, es, lr, TqdmCallback(verbose=1)])
+
+y_pred = cnn.predict_classes(test)
+
+sample_submission['Label'] = y_pred
+sample_submission.to_csv('ss.csv', index=False)
+
+cnn.save('mnist_model.h5')
+
